e1eb435a947ade38153d395ce25fee1c379f85fd
Williamwcn <williamwcn@outlook.com>
2021-06-12 17:13:07 -0300

    ok

--- Curso em Vídeo/Java Script/Aula 15/ambiente.js
+++ Curso em Vídeo/Java Script/Aula 15/ambiente.js
@@ -1,3 +1,14 @@
 let num = [5,8,2,9,3]
 
-console.log(num)
+num.push(1)                                             // Adiciona um item na ultima posição.
+num.sort()                                              // Coloca em ordem A -> Z.
+console.log(num)                                        // Exibi o vetor.
+console.log(`O valor tem ${num.length} posições`)       // Quantidade de posições. 
+console.log(`O primeiro valor do vetor é ${num[0]}`)    // Exibi o valor do vetor de acordo com a posição [].
+
+let pos = num.indexOf(8)                                // Busca a posição do valor se existir.
+if (pos == -1) {
+    console.log('O valor não foi encontrado.')
+} else {
+    console.log(`O valor está na posição ${pos}`)
+}
